0afe3b252acfaa25996045a0721402f53fcc44a2
Alex Carney <alex.rugby101@gmail.com>
2014-06-18 15:42:28 +0100

    Improved Alt Tab functionality

--- rc.lua
+++ rc.lua
@@ -351,14 +351,23 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
     awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
+
+    -- Improved Alt+Tabbing
     awful.key({ modkey,           }, "Tab",
         function ()
-            awful.client.focus.history.previous()
+            awful.client.focus.byidx(-1)
             if client.focus then
                 client.focus:raise()
             end
         end),
 
+    awful.key({ modkey, "Shift"    }, "Tab",
+        function ()
+            awful.client.focus.byidx(1)
+            if client.focus then
+                client.focus:raise()
+            end
+        end),
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
