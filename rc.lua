cc33aebb243796ca0227396b9a6f9abf914e451d
Alex Carney <alex.rugby101@gmail.com>
2014-06-25 10:28:30 +0100

    Added start calls for pulseaudio, dropbox and compton

--- rc.lua
+++ rc.lua
@@ -568,3 +568,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+awful.util.spawn_with_shell("pulseaudio --start")
+awful.util.spawn_with_shell("dropbox start")
+awful.util.spawn_with_shell("compton -b --config ~/.config/compton.conf")
