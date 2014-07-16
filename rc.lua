b996c8f6ebda5573a8fac830c6f1bf663734bee9
Alex Carney <alex.rugby101@gmail.com>
2014-07-16 13:41:15 +0100

    Updated the terminal emualtor variable

--- rc.lua
+++ rc.lua
@@ -49,7 +49,7 @@ wallpaper_dir = "/home/alex/Media/Pictures/Wallpapers/"
 beautiful.init(config_location  .."theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "termite"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
