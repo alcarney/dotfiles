6355d654ad037c2d3aced3905c4422fe7e08271d
Alex Carney <alex.rugby101@gmail.com>
2014-08-25 00:35:50 +0100

    Changed my terminal emulator -- again!

--- rc.lua
+++ rc.lua
@@ -49,7 +49,7 @@ wallpaper_dir = "/home/alex/Media/Pictures/Wallpapers/"
 beautiful.init(config_location  .."theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "termite"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
