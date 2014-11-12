8ac00f2d97f85ed348191bf73ef976cbe1beacca
Alex Carney <alex.rugby101@gmail.com>
2014-11-12 23:27:45 +0000

    Made it so we dont get horrible conky errors etc when we restart awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -612,8 +612,25 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-awful.util.spawn_with_shell("compton -b --config ~/.config/compton.conf")
-awful.util.spawn_with_shell("conky")
-awful.util.spawn_with_shell("pulseaudio --start")
-awful.util.spawn_with_shell("dropbox start")
-awful.util.spawn_with_shell("tilda")
+
+function run_once(prg,arg_string,pname,screen)
+    if not prg then
+        do return nil end
+    end
+
+    if not pname then
+       pname = prg
+    end
+
+    if not arg_string then 
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. ")",screen)
+    else
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. " ".. arg_string .."' || (" .. prg .. " " .. arg_string .. ")",screen)
+    end
+end
+
+run_once("compton -b --config ~/.config/compton.conf")
+run_once("pulseaudio --start")
+run_once("dropbox start")
+--run_once("tilda")
+run_once("urxvt -e sh -c 'tmux'")
