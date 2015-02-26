46ad3916e5b7355d5935011eafde15dad6c0cb2f
Alex Carney <alcarneyorg@gmail.com>
2015-02-26 20:23:33 +0000

    Basic dzen finally added, one day i will add popup thingys

--- dzen_widgets.sh
+++ dzen_widgets.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+dir=$(echo ${BASH_SOURCE[0]} | sed 's/dzen_widgets.sh//')
+widget_dir="$dir/dzen_widgets"
+
+calendar ()
+{
+    date=$(date +"%d/%m/%y")
+    timeis=$(date +"%H:%M:%S")
+    echo -n "$timeis | ^ca(1, . $widget_dir/calendar.sh)^fg(red)$date ^fg()^ca()"
+    return
+}
+
 status_line ()
 {
-    date +"%H:%M:%S %d/%m/%y"
+    calendar
+    echo
+    return
 }
 
 while true
