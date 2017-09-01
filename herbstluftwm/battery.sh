30d88aad9dfe45ba3dae0fc6da82892bd17398a6
Alex Carney <alcarneyme@gmail.com>
2017-09-01 18:07:02 +0100

    Silently do nothing if acpi not installed

--- herbstluftwm/battery.sh
+++ herbstluftwm/battery.sh
@@ -1,16 +1,22 @@
 # This script is responsible for retreiving battery
 # info, requires acpi
-echo -ne "power\t"
 
-acpi -b | cut -d ' ' -f 4 | tr '\n' ' ' | sed 's/[,%]//g'
-AC=$(acpi -a | cut -d ' ' -f 3)
+exists=$(which acpi 2> /dev/null)
+if [ $exists ] ; then
 
-case $AC in
-    off-line)
-        echo -n "O";;
-    on-line)
-        echo -n "C";;
-    *)
-        echo -n "?";;
-esac
-echo
+    echo -ne "power\t"
+
+    acpi -b | cut -d ' ' -f 4 | tr '\n' ' ' | sed 's/[,%]//g'
+    AC=$(acpi -a | cut -d ' ' -f 3)
+
+    case $AC in
+        off-line)
+            echo -n "O";;
+        on-line)
+            echo -n "C";;
+        *)
+            echo -n "?";;
+    esac
+    echo
+
+fi
