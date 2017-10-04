c330bc3797d8bcf85de86089e030766f4fe476fa
Alex Carney <alcarneyme@gmail.com>
2017-10-04 19:15:47 +0100

    Improvements to select_tmux_session
    - Now you can type the name of a tmuxinator project, if its not already
    a session the script will start it and attach
    - Alternatively you can type the name of a new session and the script
    will start a session in that name and attach to it for you :D

--- herbstluftwm/select_tmux_session.sh
+++ herbstluftwm/select_tmux_session.sh
@@ -13,5 +13,26 @@ sessions=$(tmux ls | cut -d ':' -f 1)
 # Pass the sessions to dmenu to get the selection
 selected=$(echo -e "$sessions" | ${dmenucmd[@]})
 
-# Spawn a new terminal and attach to the session
-${TERMINAL:-st} -e tmux attach -t $selected &
+# Was the selected item an existing session?
+echo -e "$sessions" | grep "$selected" > /dev/null 2> /dev/null
+
+if [[ "$?" == "0" ]] ; then
+
+    # Spawn a new terminal and attach to the session
+    ${TERMINAL:-st} -e tmux attach -t $selected &
+    exit
+fi
+
+# If not - is an existing tmuxinator project?
+projects=$(tmuxinator list | grep -v projects | awk 'BEGIN{RS="[ ]+";}{printf "%s\n", $1}')
+echo -e "${projects[@]}" | grep $selected > /dev/null 2> /dev/null
+
+if [[ "$?" == "0" ]]; then
+
+    # Spawn a new terminal and start the project
+    ${TERMINAL:-st} -e tmuxinator start $selected &
+    exit
+fi
+
+# If we got this far - assume I want to start a new session with this name
+${TERMINAL:-st} -e tmux new-session \; rename-session $selected &
