7b49e9edabb2eebd7cbdf68f100186b5151a6194
Alex Carney <alcarneyme@gmail.com>
2018-12-29 18:41:13 +0000

    Tweaks to how the lock screen functions

--- i3/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir="$HOME/.xlock"
-screen="$dir/screenshot.png"
-
-scrot "$screen" || exit 1
-convert "$screen" -paint 4 -grayscale Rec709Luminance "$screen"
-
-i3lock -u -i "$screen"
