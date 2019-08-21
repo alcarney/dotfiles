a2a10f187aac11ea3581ad6ef804e30cb5053ef4
Alex Carney <alcarneyme@gmail.com>
2019-08-21 19:21:50 +0100

    My bar config is has now been "literate-ised"

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# Polybar startup script taken from the github wiki
-
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar top&
-polybar bottom&
+polybar main&
