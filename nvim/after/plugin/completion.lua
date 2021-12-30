4a5025cf787966a366f4621e43d185b58c99ea3e
Alex Carney <alcarneyme@gmail.com>
2021-12-30 13:30:24 +0000

    nvim(completion) prettier completions, vscode style mappings

--- nvim/after/plugin/completion.lua
+++ nvim/after/plugin/completion.lua
@@ -1,4 +1,12 @@
-local cmp = require('cmp')
+local cmp_ok, cmp = pcall(require, 'cmp')
+if not cmp_ok then
+  return
+end
+
+local luasnip_ok, luasnip = pcall(require, 'luasnip')
+if not luasnip_ok then
+  return
+end
 
 local kinds = {
   Text = '  ',
@@ -28,15 +36,49 @@ local kinds = {
   TypeParameter = '  ',
 }
 
+-- Taken from the Luasnip "super tab" example:
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
   -- VSCode style completion items:
   -- https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-codicons-to-the-menu
   formatting = {
-    fields = { "kind", "abbr" },
-    format = function(_, vim_item)
+    fields = { "kind", "abbr", "menu"},
+    format = function(entry, vim_item)
       vim_item.kind = kinds[vim_item.kind] or ""
       return vim_item
     end
+  },
+  mapping = {
+    ["<c-space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+    ["<tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.confirm({select = false})
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
+
+    ["<s-tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {"i", "s"}),
   },
   sources = {
     { name = "nvim_lsp" }
