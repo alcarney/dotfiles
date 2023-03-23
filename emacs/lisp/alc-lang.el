9a927df5c0a8bea49db3addadee9ac6b597cc465
Alex Carney <alcarneyme@gmail.com>
2023-03-23 20:37:45 +0000

    emacs: Smarter mechanism for finding venvs

--- emacs/lisp/alc-lang.el
+++ emacs/lisp/alc-lang.el
@@ -12,13 +12,24 @@
       '(isort black))
   (apheleia-global-mode))
 
+(defun me/pyright-find-venv-path (dirname venv)
+  "Search upwards from DIRNAME looking for VENV."
+  (if (file-exists-p (file-name-concat dirname venv))
+      dirname
+    (let ((parent (file-name-directory (directory-file-name dirname))))
+      (if (not (string= "/" parent))
+          (me/pyright-find-venv-path parent venv)))))
+
 (defun me/eglot-python-workspace-config (server)
   "Configure pyright to use the correct virtual environment."
-  (message "Finding venv path...")
   (if (project-current)
-      (let ((venv-path (project-root (project-current))))
-        (list (cons :python
-              (list :venvPath (expand-file-name venv-path)))))
+      (let* ((dirname   (expand-file-name (project-root (project-current))))
+             (venv-path (me/pyright-find-venv-path dirname ".env")))
+        (if venv-path
+            (list (cons :python
+                        (list :venvPath venv-path)))
+                  (cons :python.analysis
+                        (list :logLevel "trace"))))
     '()))
 
 (use-package eglot
