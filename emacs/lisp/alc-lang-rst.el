e4198dfee7b4453bac85432ea6ca1dc4e64fe179
Alex Carney <alcarneyme@gmail.com>
2023-06-26 21:22:26 +0100

    emacs: Enable `eglot` in `rst-mode` buffers

--- emacs/lisp/alc-lang-rst.el
+++ emacs/lisp/alc-lang-rst.el
@@ -1,7 +1,13 @@
 ;;; alc-lang-rst.el --- Settings for reStructuredText files
 
 (use-package rst
-  :hook (rst-mode . visual-line-mode))
+  :hook ((rst-mode . eglot-ensure)
+         ;; TODO: Figure out how to prevent flyspell's default keybindings from
+         ;;       conflicting with the `completion-at-point' binding we want.
+         ;; (rst-mode . flyspell-mode)
+         (rst-mode . visual-line-mode))
+  :bind (:map rst-mode-map
+              ("C-M-i" . completion-at-point)))
 
 
 
