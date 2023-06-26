292ee7771443282421ee202bef604b1a8137bc5d
Alex Carney <alcarneyme@gmail.com>
2023-06-26 21:23:43 +0100

    emacs: custom.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0af4fc8329f73bd771df30015858813385461513e044df730fc805a49f5ece52" default))
  '(package-selected-packages
-   '(ef-themes apheleia yaml-mode eglot nix-mode kind-icon corfu embark orderless vertico minions magit use-package)))
+   '(rustic solaire-mode xref denote markdown-mode ef-themes apheleia yaml-mode eglot nix-mode kind-icon corfu embark orderless vertico minions magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
