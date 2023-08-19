b0e23efb8e25748cdb70061a101bfd9ad35d2799
Alex Carney <alcarneyme@gmail.com>
2023-08-19 10:09:45 +0100

    home-manager: Manage git config

--- home.nix
+++ home.nix
@@ -48,11 +48,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
+    ".gitconfig".source = ./gitconfig;
   };
 
   # You can also manage environment variables but you will have to manually
