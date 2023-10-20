6a9cb790a1bfed8d45a084edfd4af0761b81ee57
Alex Carney <alcarneyme@gmail.com>
2023-10-20 22:16:00 +0100

    emacs: Switch back to vertico
    The built in completion experiment was almost ideal but fell over when
    it came to more interactive commands like `consult-*`.
    
    This commit re-enabled `vertico-mode` but also makes use of
    `vertico-multiform-mode`. I now use the unobtrusive UI by default,
    with the option of enabling fancier displays when necessary.

--- emacs/lisp/alc-minibuffer.el
+++ emacs/lisp/alc-minibuffer.el
@@ -19,24 +19,27 @@
       completions-max-height 15)
 
 ;; Keybindings
-(define-key minibuffer-local-map (kbd "C-p") #'minibuffer-previous-completion)
-(define-key minibuffer-local-map (kbd "C-n") #'minibuffer-next-completion)
+;; (define-key minibuffer-local-map (kbd "C-p") #'minibuffer-previous-completion)
+;; (define-key minibuffer-local-map (kbd "C-n") #'minibuffer-next-completion)
 
-(define-key completion-in-region-mode-map (kbd "C-p") #'minibuffer-previous-completion)
-(define-key completion-in-region-mode-map (kbd "C-n") #'minibuffer-next-completion)
+;; (define-key completion-in-region-mode-map (kbd "C-p") #'minibuffer-previous-completion)
+;; (define-key completion-in-region-mode-map (kbd "C-n") #'minibuffer-next-completion)
 
 ;; Filter commands according to major mode
 (setq read-extended-command-predicate
       #'command-completion-default-include-p)
 
-;; (use-package vertico
-;;   :init
-;;   (vertico-mode 1)
+(use-package vertico
+  :custom
+  (vertico-multiform-categories '((t unobtrusive)))
+  :config
+  (vertico-mode 1)
+  (vertico-multiform-mode 1)
 
-;;   ;; When calling `find-file' automatically clear the filepath prompt
-;;   ;; when typing something that "resets" it e.g. '/' or '~/'
-;;   (file-name-shadow-mode 1)
-;;   (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy))
+  ;; When calling `find-file' automatically clear the filepath prompt
+  ;; when typing something that "resets" it e.g. '/' or '~/'
+  (file-name-shadow-mode 1)
+  (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy))
 
 (use-package savehist
   ;; Vertico uses this to sort by most recently used.
