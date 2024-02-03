7284b5dcba26c78b58ed904bb24b2ad477ce4c1d
Alex Carney <alcarneyme@gmail.com>
2024-02-03 23:19:05 +0000

    emacs: Use :custom

--- emacs/lisp/alc-lang-rust.el
+++ emacs/lisp/alc-lang-rust.el
@@ -1,8 +1,8 @@
 ;;; alc-lang-rust.el --- Settings for rust files -*- lexical-binding: t -*-
 
 (use-package rustic
-  :config
-  (setq rustic-lsp-client 'eglot
-        rustic-analyzer-command '("nix" "develop" "-c" "rust-analyzer")))
+  :custom
+  (rustic-lsp-client 'eglot)
+  (rustic-analyzer-command '("nix" "develop" "-c" "rust-analyzer")))
 
 (provide 'alc-lang-rust)
