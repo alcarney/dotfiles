b29afb78323e55a6b16841ce4264ffad58ecc5d0
Alex Carney <alcarneyme@gmail.com>
2024-03-18 19:34:41 +0000

    emacs: Update `display-buffer-alist` rules

--- emacs/lisp/alc-window.el
+++ emacs/lisp/alc-window.el
@@ -16,55 +16,60 @@
           (slot . 0)
           (dedicated . t)))
 
-(setopt display-buffer-alist
-        `(
-          ;; Shells, eshells, inferior-python-mode and more
-          ((derived-mode . comint-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 0)
-           (dedicated . t))
+(setq display-buffer-alist
+      `(
+        ;; forge.el posts and replies
+        ((derived-mode . forge-post-mode)
+         (display-buffer-below-selected)
+         (dedicated . t))
 
-          ;; eat terminals
-          ((derived-mode . eat-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 0)
-           (dedicated . t))
+        ;; Shells, eshells, inferior-python-mode and more
+        ((derived-mode . comint-mode)
+         (display-buffer-reuse-mode-window display-buffer-in-side-window)
+         (side . bottom)
+         (slot . 0)
+         (dedicated . t))
 
-          ;; Compilation buffers
-          ((derived-mode . compilation-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 0)
-           (dedicated . t))
+        ;; eat terminals
+        ((derived-mode . eat-mode)
+         (display-buffer-reuse-mode-window display-buffer-in-side-window)
+         (side . bottom)
+         (slot . 0)
+         (dedicated . t))
 
-          ;; Flymake buffers
-          ((derived-mode . flymake-diagnostics-buffer-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 0)
-           (dedicated . t))
+        ;; Compilation buffers
+        ((derived-mode . compilation-mode)
+         (display-buffer-reuse-mode-window display-buffer-in-side-window)
+         (side . bottom)
+         (slot . 0)
+         (dedicated . t))
 
-          ((derived-mode . flymake-project-diagnostics-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 0)
-           (dedicated . t))
+        ;; Flymake buffers
+        ((derived-mode . flymake-diagnostics-buffer-mode)
+         (display-buffer-reuse-mode-window display-buffer-in-side-window)
+         (side . bottom)
+         (slot . 0)
+         (dedicated . t))
 
-          ;; magit-process
-          ((derived-mode . magit-process-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . bottom)
-           (slot . 0)
-           (dedicated . t))
+        ((derived-mode . flymake-project-diagnostics-mode)
+         (display-buffer-reuse-mode-window display-buffer-in-side-window)
+         (side . bottom)
+         (slot . 0)
+         (dedicated . t))
 
-          ;; dired
-          ((derived-mode . dired-mode)
-           (display-buffer-reuse-mode-window display-buffer-in-side-window)
-           (side . left)
-           (slot . 0)
-           (dedicated . t))
-          ))
+        ;; magit-process
+        ((derived-mode . magit-process-mode)
+         (display-buffer-reuse-mode-window display-buffer-in-side-window)
+         (side . bottom)
+         (slot . 0)
+         (dedicated . t))
+
+        ;; dired
+        ;; ((derived-mode . dired-mode)
+        ;;  (display-buffer-reuse-mode-window display-buffer-in-side-window)
+        ;;  (side . left)
+        ;;  (slot . 0)
+        ;;  (dedicated . t))
+        ))
 
 (provide 'alc-window)
