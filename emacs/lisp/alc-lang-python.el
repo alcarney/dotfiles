d68fbdff89beedb3d363c60b9ba0f87c0f4ad6fb
Alex Carney <alcarneyme@gmail.com>
2024-03-18 19:31:09 +0000

    emacs: Make python files in `site-packages/` read only by default

--- emacs/lisp/alc-lang-python.el
+++ emacs/lisp/alc-lang-python.el
@@ -1,12 +1,18 @@
 ;;; alc-lang-python.el --- Python configuration
 
-(defun my/python-mode-hook ()
+(defun alc-python-mode-hook ()
   "Tweaks and config to run when starting `python-mode'"
   (setq-local fill-column 88)
-  (eglot-ensure))
+
+  ;; Files in site-packages/ should make read only by default.
+  ;; TODO: Include system lib/ and typeshed-fallback/ in this
+  (if (string-match-p "site-packages/" (buffer-file-name))
+      (read-only-mode)
+    (eglot-ensure)))
 
 (use-package python
-  :hook (python-mode . my/python-mode-hook)
+  :hook ((python-mode . alc-python-mode-hook)
+         (python-ts-mode . alc-python-mode-hook))
   :config
   (setq python-shell-dedicated 'project))
 
