61f4121ab0e6d903890955dae781db51a5ef3b48
Alex Carney <alcarneyme@gmail.com>
2024-04-16 19:43:18 +0100

    emacs: Enable eat in eshell

--- emacs/lisp/alc-terminals.el
+++ emacs/lisp/alc-terminals.el
@@ -1,6 +1,7 @@
 ;;; alc-terminals.el --- (e)Shell, term, eat, oh my! -*- lexical-binding: t -*-
 
 (use-package eat
+  :hook ((eshell-load . eat-eshell-mode))
   :config
   (with-eval-after-load 'project
     (keymap-set project-prefix-map "t" #'eat-project)))
