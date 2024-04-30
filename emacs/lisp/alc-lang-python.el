2154678634b9a041109515073e0506b736960a67
Alex Carney <alcarneyme@gmail.com>
2024-04-30 18:59:40 +0100

    emacs: Better detection of non-project files

--- emacs/lisp/alc-lang-python.el
+++ emacs/lisp/alc-lang-python.el
@@ -1,12 +1,30 @@
 ;;; alc-lang-python.el --- Python configuration
 
+(defun alc-python-library-file-p ()
+  "Determine if the current buffer is a library file"
+  (if-let ((file-name (buffer-file-name)))
+      (or
+       (string-match-p "site-packages/" file-name)
+       (string-match-p "typeshed-fallback/" file-name)
+       (string-match-p "/usr/lib\\(64\\)?/" file-name))
+    ;; For now, consider buffers that do not visit a file a "library" as well
+    t))
+
+(defun alc-python-get-interpreter-path ()
+  "Locate the path to the python interpreter for the current file"
+  (when-let ((root (locate-dominating-file default-directory ".env"))
+             (python (file-name-concat root ".env/bin/python"))
+             (_ (file-exists-p python)))
+    python))
+
 (defun alc-python-mode-hook ()
   "Tweaks and config to run when starting `python-mode'"
   (setq-local fill-column 88)
 
-  ;; Files in site-packages/ should make read only by default.
-  ;; TODO: Include system lib/ and typeshed-fallback/ in this
-  (if (string-match-p "site-packages/" (buffer-file-name))
+  ;; Files in site-packages/ etc. should be read only by default.
+  ;; Also do not start eglot in these locations to cut down on the
+  ;; number of server instances.
+  (if (alc-python-library-file-p)
       (read-only-mode)
     (eglot-ensure)))
 
